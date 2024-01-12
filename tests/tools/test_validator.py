44b543a3c93fe7dd3a5f306383ed2f6e30393c95
Greg Oliveira <gregoryomelo@gmail.com>
2024-01-12 03:03:03 -0300

    test(test_validator): create a class to validate_sublists function

--- tests/tools/test_validator.py
+++ tests/tools/test_validator.py
@@ -39,67 +39,70 @@ def list_with_unequal_elements_and_unbalance():
     ]
 
 
-def test_equal_elements_and_order(list_with_equal_elements_equal_order):
-    """
-    Test validate_sublists function with lists having the same elements
-    in the same order.
+class TestValidateSublists:
+    def test_equal_elements_and_order(
+        self,
+        list_with_equal_elements_equal_order,
+    ):
+        """
+        Test validate_sublists function with lists having the same elements
+        in the same order.
 
-    Asserts that the function returns True for lists with identical elements
-    and order.
+        Asserts that the function returns True for lists with identical
+        elements and order.
 
-    Parameters:
-    list_with_equal_elements_equal_order (list): The input provided by
-    a fixture.
-    """
-    assert validate_sublists(list_with_equal_elements_equal_order)
+        Parameters:
+        list_with_equal_elements_equal_order (list): The input provided by
+        a fixture.
+        """
+        assert validate_sublists(self, list_with_equal_elements_equal_order)
 
+    def test_equal_elements_and_different_order(
+        self,
+        list_with_equal_elements_different_order,
+    ):
+        """
+        Test validate_sublists function with lists having the same elements in
+        different order.
 
-def test_equal_elements_and_different_order(
-    list_with_equal_elements_different_order,
-):  # noqc
-    """
-    Test validate_sublists function with lists having the same elements in
-    different order.
+        Asserts that the function returns True for lists with identical
+        elements regardless of their order.
 
-    Asserts that the function returns True for lists with identical elements
-    regardless of their order.
+        Parameters:
+        list_with_equal_elements_different_order (list): The input provided by
+        a fixture.
+        """
+        assert validate_sublists(list_with_equal_elements_different_order)
 
-    Parameters:
-    list_with_equal_elements_different_order (list): The input provided by
-    a fixture.
-    """
-    assert validate_sublists(list_with_equal_elements_different_order)
+    def test_unequal_elements(self, list_with_unequal_elements):
+        """
+        Test validate_sublists function with lists having different elements.
 
+        Asserts that the function raises a ValueError for lists with differing
+        elements.
 
-def test_unequal_elements(list_with_unequal_elements):
-    """
-    Test validate_sublists function with lists having different elements.
+        Parameters:
+        list_with_unequal_elements (list): The input provided by a fixture.
+        """
+        with pytest.raises(ValueError) as excinfo:
+            validate_sublists(list_with_unequal_elements)
+        assert "Sublists do not have the same elements." in str(excinfo.value)
 
-    Asserts that the function raises a ValueError for lists with differing
-    elements.
+    def test_unequal_elements_and_unbalance(
+        self,
+        list_with_unequal_elements_and_unbalance,
+    ):
+        """
+        Test validate_sublists function with lists having different elements
+        and varying lengths.
 
-    Parameters:
-    list_with_unequal_elements (list): The input provided by a fixture.
-    """
-    with pytest.raises(ValueError) as excinfo:
-        validate_sublists(list_with_unequal_elements)
-    assert "Sublists do not have the same elements." in str(excinfo.value)
+        Asserts that the function raises a ValueError for lists with differing
+        elements and lengths.
 
-
-def test_unequal_elements_and_unbalance(
-    list_with_unequal_elements_and_unbalance,
-):  # noqc
-    """
-    Test validate_sublists function with lists having different elements and
-    varying lengths.
-
-    Asserts that the function raises a ValueError for lists with differing
-    elements and lengths.
-
-    Parameters:
-    list_with_unequal_elements_and_unbalance (list): The input provided by
-    a fixture.
-    """
-    with pytest.raises(ValueError) as excinfo:
-        validate_sublists(list_with_unequal_elements_and_unbalance)
-    assert "Sublists do not have the same elements." in str(excinfo.value)
+        Parameters:
+        list_with_unequal_elements_and_unbalance (list): The input provided by
+        a fixture.
+        """
+        with pytest.raises(ValueError) as excinfo:
+            validate_sublists(list_with_unequal_elements_and_unbalance)
+        assert "Sublists do not have the same elements." in str(excinfo.value)
